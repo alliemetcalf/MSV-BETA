5a3de530f8f216459398879f86ec47b0a03d1505
CommentAIry <commentairyai@gmail.com>
2025-12-09 21:52:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "msv-beta",
+  "appId": "1:614074080882:web:8f256761b55914053d2ceb",
+  "apiKey": "AIzaSyBCG29EhICnZf3cjUYq4Hvn2q45ENB2rpQ",
+  "authDomain": "msv-beta.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "614074080882"
+};
