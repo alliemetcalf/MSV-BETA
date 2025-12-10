4e9cb183bf7b152c1029fb43e74a7dc4627beba3
CommentAIry <commentairyai@gmail.com>
2025-12-10 01:42:28 +0000

    this is the right app to use: // Import the functions you need from the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "msv-beta",
-  "appId": "1:614074080882:web:8f256761b55914053d2ceb",
-  "storageBucket": "msv4.appspot.com",
   "apiKey": "AIzaSyBCG29EhICnZf3cjUYq4Hvn2q45ENB2rpQ",
   "authDomain": "msv-beta.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "614074080882"
+  "projectId": "msv-beta",
+  "storageBucket": "msv-beta.appspot.com",
+  "messagingSenderId": "614074080882",
+  "appId": "1:614074080882:web:1e1984151011c1323d2ceb",
+  "measurementId": "G-6L55TC2D5Y"
 };
-
