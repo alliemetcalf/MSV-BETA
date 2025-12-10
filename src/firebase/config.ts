dc7adcaa344d1ca4a26bcf1174d0c43a82870930
CommentAIry <commentairyai@gmail.com>
2025-12-10 00:34:13 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
+
 export const firebaseConfig = {
-  "projectId": "msv-beta",
+  "projectId": "msv-beta2",
   "appId": "1:614074080882:web:8f256761b55914053d2ceb",
-  "storageBucket": "msv-beta.appspot.com",
+  "storageBucket": "msv-beta2.appspot.com",
   "apiKey": "AIzaSyBCG29EhICnZf3cjUYq4Hvn2q45ENB2rpQ",
-  "authDomain": "msv-beta.firebaseapp.com",
+  "authDomain": "msv-beta2.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "614074080882"
 };
