79dad05b4a14f32f09773fc546f1a763a4b2951f
CommentAIry <commentairyai@gmail.com>
2025-12-10 01:35:39 +0000

    no - create a NEW storage bucket - call it msv4 and then use that bucket

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "msv-beta",
   "appId": "1:614074080882:web:8f256761b55914053d2ceb",
-  "storageBucket": "msv-beta.appspot.com",
+  "storageBucket": "msv4.appspot.com",
   "apiKey": "AIzaSyBCG29EhICnZf3cjUYq4Hvn2q45ENB2rpQ",
   "authDomain": "msv-beta.firebaseapp.com",
   "measurementId": "",
