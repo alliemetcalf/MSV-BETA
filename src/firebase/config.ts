df6145d00f3e9183afe7e3ba0fa3735ca17014aa
CommentAIry <commentairyai@gmail.com>
2025-12-10 02:04:47 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBCG29EhICnZf3cjUYq4Hvn2q45ENB2rpQ",
   authDomain: "msv-beta.firebaseapp.com",
   projectId: "msv-beta",
-  storageBucket: "msv-beta.appspot.com",
+  storageBucket: "msv-beta.firebasestorage.app",
   messagingSenderId: "614074080882",
   appId: "1:614074080882:web:1e1984151011c1323d2ceb",
   measurementId: "G-6L55TC2D5Y"
