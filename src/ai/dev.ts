aff40f70ae1014b581f8193f1a1140b3cdd15284
CommentAIry <commentairyai@gmail.com>
2025-12-10 23:40:38 +0000

    did the flow execute?

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -4,4 +4,3 @@ import './flows/create-user-flow';
 import './flows/list-users-flow';
 import './flows/update-user-role-flow';
 import './flows/get-all-door-codes-flow';
-import './flows/clear-notice-date-flow';
