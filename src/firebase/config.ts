5046e36961f09bfaf93784115aef3e5a1190d3ff
CommentAIry <commentairyai@gmail.com>
2025-12-10 00:36:59 +0000

    undo that.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
 
 export const firebaseConfig = {
-  "projectId": "msv-beta2",
+  "projectId": "msv-beta",
   "appId": "1:614074080882:web:8f256761b55914053d2ceb",
-  "storageBucket": "msv-beta2.appspot.com",
+  "storageBucket": "msv-beta.appspot.com",
   "apiKey": "AIzaSyBCG29EhICnZf3cjUYq4Hvn2q45ENB2rpQ",
-  "authDomain": "msv-beta2.firebaseapp.com",
+  "authDomain": "msv-beta.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "614074080882"
 };
+
