85e458a5933f40b2ea6a0c1bdc0409ed4254fcbd
CommentAIry <commentairyai@gmail.com>
2025-12-10 23:39:30 +0000

    make all tenant record fields of "30 day notice" with a date of december

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -4,3 +4,4 @@ import './flows/create-user-flow';
 import './flows/list-users-flow';
 import './flows/update-user-role-flow';
 import './flows/get-all-door-codes-flow';
+import './flows/clear-notice-date-flow';
