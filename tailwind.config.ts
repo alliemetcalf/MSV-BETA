7ef06a0a7124182eb0e73e508ea72c8b663a7f24
CommentAIry <commentairyai@gmail.com>
2025-12-11 17:04:42 +0000

    just change the fonts, let's see if you can affect that first.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['"Roboto Mono"', 'monospace'],
+        headline: ['Lobster', 'cursive'],
         code: ['monospace'],
       },
       colors: {
