2fad08aca28dca89304732aa84b36724eff5fa0b
CommentAIry <commentairyai@gmail.com>
2025-12-11 17:50:27 +0000

    fix this layout. ONLY this layout on this page.

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as React from 'react';
+import 'react-day-picker/dist/style.css';
 import { Calendar } from '@/components/ui/calendar';
 import { MainLayout } from '@/components/MainLayout';
 
